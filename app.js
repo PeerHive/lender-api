98d826c0dd7540b834756566a0a1d19ae2b72559
vincent_yeo <vincent.yeo@peerhive.app>
2023-07-13 09:42:58 +0800

    Debug process

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const auth = require('./middlewares/authMiddleware');
 
 
 const mongo_Connection = process.env.CONNECTION_URL;
-consol.log(mongo_Connection)
+console.log(mongo_Connection)
 
 // Connect to the MongoDB database
 
